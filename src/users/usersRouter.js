8a8d6cacaccb3000587771b058e2405c39da3854
paulstgermain <paulg.stgermain@gmail.com>
2023-07-21 15:25:46 -0400

    fixes getAllUsers route

--- src/users/usersRouter.js
+++ src/users/usersRouter.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Users = require('./usersModel');
 
-// Create new user
-router.post('/create', function (req, res, next) {
-    Users.createUser(req.body)
+// Get all users
+router.get('/', function (req, res) {
+    Users.getAllUsers()
         .then((response) => {
             res.status(200).json(response);
         })
@@ -13,11 +13,11 @@ router.post('/create', function (req, res, next) {
         })
 })
 
-// Get all users
-router.get('/', function (req, res, next) {
-    Users.getAllUsers()
+// Create new user
+router.post('/create', function (req, res) {
+    Users.createUser(req.body)
         .then((response) => {
-            res.json(200).json(response);
+            res.status(200).json(response);
         })
         .catch((err) => {
             res.status(err.status).json({ message: err.message });
