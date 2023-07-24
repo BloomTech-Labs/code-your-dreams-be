d10c265c35d0edbc161475257f3462f030f08564
paulstgermain <paulg.stgermain@gmail.com>
2023-07-24 13:36:56 -0400

    adds courseMaterialsModel and courseMaterialsRouter

---
+++ src/courseMaterials/courseMaterialsModel.js
@@ -0,0 +1,38 @@
+const db = require('../data/db-config');
+
+// CREATE NEW COURSE MATERIAL
+const createCourseMaterial = (courseMaterialData) => {
+    return db('course_materials').insert(courseMaterialData).returning('*');
+}
+
+// GET ALL COURSE MATERIALS
+const getAllCourseMaterials = () => {
+    return db('course_materials');
+}
+
+// GET COURSE MATERIAL BY ID
+const getCourseMaterialById = (id) => {
+    return db('course_materials').where({ auth0_id: id }).first().select('*');
+}
+
+// UPDATE COURSE MATERIAL
+const updateCourseMaterial = (id, newCourseMaterialData) => {
+    return db('course_materials')
+        .where({ auth0_id: id })
+        .first()
+        .update(newCourseMaterialData)
+        .returning('*');
+}
+
+// DELETE COURSE MATERIAL
+const deleteCourseMaterial = (id) => {
+    return db('course_materials').where({ id }).del();
+}
+
+module.exports = {
+    createCourseMaterial,
+    getAllCourseMaterials,
+    getCourseMaterialById,
+    updateCourseMaterial,
+    deleteCourseMaterial
+};
